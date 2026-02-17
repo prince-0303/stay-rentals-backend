5b12524861aed1ec908b6e8882ca1886681e48a2
prince-0303 <princebinil2003@gmail.com>
2026-02-18 00:18:32 +0530

    updated urls with mfa

--- backend_service/backend_service/urls.py
+++ backend_service/backend_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth_app.urls')),
+    path('api/auth/', include('auth_app.urls')),
 ]
